811238288100b8ae9cd9d7b9a27b27e3e1b9d47c
BrookeBryski <brookeabryski@gmail.com>
2022-01-28 15:27:19 -0600

    initial commit

---
+++ src/main/java/com/example/gsaccessingjpadatawithrest/GsAccessingJpaDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingjpadatawithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsAccessingJpaDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsAccessingJpaDataWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gsaccessingjpadatawithrest/GsAccessingJpaDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsaccessingjpadatawithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingJpaDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
