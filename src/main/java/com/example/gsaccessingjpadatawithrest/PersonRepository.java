9d3a247771838d6968dcbab12fd6fd55a9363723
BrookeBryski <brookeabryski@gmail.com>
2022-01-31 13:44:01 -0600

    person repository

---
+++ src/main/java/com/example/gsaccessingjpadatawithrest/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.example.gsaccessingjpadatawithrest;
+// This repository is an interface that lets you perform various operations involving Person objects
+
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+
+}
